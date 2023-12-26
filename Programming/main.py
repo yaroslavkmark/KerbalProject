44dfbb8fe83766a8da4a1b95bdcfea245dfafe16
yaroslavkmark <87874420+yaroslavkmark@users.noreply.github.com>
2023-12-26 21:40:35 +0300

    Update main.py

--- Programming/main.py
+++ Programming/main.py
@@ -5,8 +5,8 @@ from time import sleep
 def main():
     conn = krpc.connect(
         name="Pink Floyd",
-        address='192.168.1.82',
-        rpc_port=50000, stream_port=50001
+        address='127.0.0.1',
+        rpc_port=500, stream_port=5001
     )
     print("SERVER STATUS: OK")
 
@@ -14,12 +14,10 @@ def main():
 
     time = 0
 
-    with open("air-speed.txt", "w") as file:
-        file.write(f"0, 0\n")
+    with open("srf-speed.txt", "w") as file:
         file.close()
 
     with open("mass-time.txt", 'w') as file:
-        file.write(f"0, 0\n")
         file.close()
 
     with open("impulse-time.txt", 'w') as file:
@@ -34,39 +32,31 @@ def main():
         file.write(f"0, 0\n")
         file.close()
 
-    with open("thrust-time.txt", 'w') as file:
-        file.write(f'0, 0\n')
-        file.close()
-
     flag_activate = False
     start_time = 0
 
-    # vx, vy, v, angels-time, mass-time, impulse-time
+    # vx, vy, v, angels-time, mass-time, impulse-time        
     while True:
         refframe = vessel.orbit.body.reference_frame
         flight_info = vessel.flight(reference_frame=refframe)
 
-        air_speed = int(flight_info.true_air_speed)
+        srf_frame = vessel.orbit.body.reference_frame
+        srf_speed = vessel.flight(srf_frame).speed
         mass = vessel.mass
-        thrust = vessel.thrust
         dry_mass = vessel.dry_mass
         impulse = int(vessel.specific_impulse)
         horizontal_speed = int(flight_info.horizontal_speed)
         vertical_speed = int(flight_info.vertical_speed)
 
-        if air_speed != 0:
+        if impulse != 0:
             ut = int(conn.space_center.ut)
             if flag_activate == False:
                 flag_activate = True
                 start_time = ut
             ut = ut - start_time
 
-            with open("resources.txt", "a") as file:
-                file.write(f"{ut}, {mass - dry_mass}")
-                file.close()
-
-            with open("air-speed.txt", "a") as file:
-                file.write(f"{ut}, {air_speed}\n")
+            with open("srf-speed.txt", "a") as file:
+                file.write(f"{ut}, {srf_speed}\n")
                 file.close()
 
             with open("mass-time.txt", 'a') as file:
@@ -83,10 +73,6 @@ def main():
 
             with open("vy-time.txt", "a") as file:
                 file.write(f'{ut}, {vertical_speed}\n')
-                file.close()
-
-            with open("thrust-time.txt", 'a') as file:
-                file.write(f'{ut}, {thrust}\n')
                 file.close()
 
             time += 2
