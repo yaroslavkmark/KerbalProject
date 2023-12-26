3463e151437d354aca04cbacef6714745a9f3183
yaroslavkmark <87874420+yaroslavkmark@users.noreply.github.com>
2023-12-26 21:39:20 +0300

    Update graph_mass-to-time.py

--- Programming/graph_mass-to-time.py
+++ Programming/graph_mass-to-time.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import numpy as np
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
@@ -17,16 +18,18 @@ def animate(i):
             x, y = line.split(', ')
             xs.append(float(x))
             ys.append(float(y))
-            
         except:
             pass
 
     ax.clear()
-    ax.plot(xs, ys)
-
-    plt.xlabel('t')
-    plt.ylabel('m')
+    X = np.arange(0, 80)
+    z = 350000 - 3015.075 * X
+    ax.plot(xs, ys, color='r', label='ksp')
+    ax.plot(X, z, color='b', label='Мат модель')
+    plt.xlabel('Время, с')
+    plt.ylabel('Масса, кг')
     plt.title('График массы ко времени полета')
+    plt.legend()
 
 
 ani = animation.FuncAnimation(fig, animate, interval=1000)
