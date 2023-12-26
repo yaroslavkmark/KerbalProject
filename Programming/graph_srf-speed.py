9d386aceb75ada930adb4c2d9cfa893d7ee8b014
yaroslavkmark <87874420+yaroslavkmark@users.noreply.github.com>
2023-12-26 21:38:52 +0300

    Update and rename graph_airspeed.py to graph_srf-speed.py

--- Programming/graph_srf-speed.py
+++ Programming/graph_srf-speed.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import numpy as np
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
 
 def animate(i):
-    data = open('air-speed.txt', 'r').read()
+    data = open('srf-speed.txt', 'r').read()
     lines = data.split('\n')
 
     xs = []
@@ -21,11 +22,14 @@ def animate(i):
             pass
 
     ax.clear()
-    ax.plot(xs, ys)
-
-    plt.xlabel('t')
-    plt.ylabel('v')
-    plt.title('График скорости ко времени полета')
+    X = np.arange(0, 80)
+    z = 150 * 9.816 * np.log(350000 / (350000 - 3015.075 * X))
+    ax.plot(xs, ys, color='r', label='ksp')
+    ax.plot(X, z, color='g', label='Мат Модель')
+    plt.xlabel('Время, с')
+    plt.ylabel('Скорость, м/c')
+    plt.title('График srf скорости ко времени полета')
+    plt.legend()
 
 
 ani = animation.FuncAnimation(fig, animate, interval=1000)
